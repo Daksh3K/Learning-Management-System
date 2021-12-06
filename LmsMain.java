eab90e459f03849a70fb48c250d741f14fcca483
YKim0172 <89658307+YKim0172@users.noreply.github.com>
2021-12-06 17:50:11 -0500

    Added additional methods that help with managing stored data

--- LmsMain.java
+++ LmsMain.java
@@ -5,39 +5,33 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class LmsMain {
+    private static ArrayList<Socket> connectedUsers = new ArrayList<>();
     public static void main(String[] args) throws IOException {
+
         ServerSocket serverSocket = new ServerSocket(1234);
         if (getPeopleListInFile() == null) {
             ArrayList<People> newArrayList = new ArrayList<>();
             updatePeopleListInFile(newArrayList);
         }
+        if (getCoursesInFile() == null) {
+            ArrayList<Course> newCourseList = new ArrayList<>();
+            updateCoursesInFile(newCourseList);
+        }
+
 
         while (true) {
             Socket socket = serverSocket.accept();
+            //add socket connected to arrayList connectedUsers
+            //connectedUsers.add(socket);
+
+
+
+
             EchoServer server = new EchoServer(socket);
             new Thread(server).start();
             System.out.println("Connection made");
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public static ArrayList<People> getPeopleListInFile() {
         try (ObjectInputStream oos = new ObjectInputStream(new FileInputStream("People.txt"))) {
@@ -63,4 +57,65 @@ public class LmsMain {
             e.printStackTrace();
         }
     }
+
+
+
+
+
+
+
+    public static ArrayList<Course> getCoursesInFile() {
+        try (ObjectInputStream oos = new ObjectInputStream(new FileInputStream("Courses.txt"))) {
+            Object o = oos.readObject();
+            if (o == null) {
+                return null;
+            }
+            ArrayList<Course> theCourses = (ArrayList<Course>) o;
+            return theCourses;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Method writes the ArrayList of courses into
+     * the file associated with the teacher.
+     * @param newArrayList
+     */
+    public static void updateCoursesInFile(ArrayList<Course> newArrayList) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Courses.txt"))) {
+            oos.writeObject(newArrayList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static ArrayList<Quiz> getQuizListInFile(String courseTextFileName) {
+        try (ObjectInputStream oos = new ObjectInputStream(new FileInputStream(courseTextFileName))) {
+            Object o = oos.readObject();
+            if (o == null) {
+                return null;
+            }
+            ArrayList<Quiz> theQuiz = (ArrayList<Quiz>) o;
+            return theQuiz;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Method puts the new ArrayList of quiz objects into the
+     * course's text file
+     * @param newArrayList
+     */
+    public static void updateQuizInFile(ArrayList<Quiz> newArrayList, String courseTextFileName) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(courseTextFileName))) {
+            oos.writeObject(newArrayList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
