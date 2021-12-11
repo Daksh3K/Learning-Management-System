4551df5cf86190fdd9b4e50342bcddc4e4c38e77
ColePriser <cpriser@purdue.edu>
2021-12-11 15:04:58 -0500

    Fully documented People.java

--- People.java
+++ People.java
@@ -4,22 +4,23 @@ import java.util.ArrayList;
 /**
  * The People class
  * Parent class of Teacher and Student
- *
+ * <p>
  * The Teachers' and Students' names are stored
- * @version November 13 2021
+ *
  * @author Yu Hyun Kim
+ * @version December 11 2021
  */
-
 public class People implements Serializable {
     private String name;  //name of Teacher or Student
     private String email;  //email of Teacher or Student
-    private String textFileName;
+    private String textFileName; //the text file that stores person
 
     /**
      * constructor for People class
      * instantiates the name and email instance variables
-     * @param name
-     * @param email
+     *
+     * @param name  the name of the person
+     * @param email the email of the person
      */
     public People(String name, String email) {
         this.name = name;
@@ -28,24 +29,44 @@ public class People implements Serializable {
     }
 
     /**
-     * getter methods for getting the value of name and email instance variables
+     * Method to get the name of the person
+     *
+     * @return String
      */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method to get the email of the person
+     *
+     * @return String
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Method to get the text file associated with the person
+     *
+     * @return String
+     */
     public String getTextFileName() {
         return textFileName;
     }
 
+    /**
+     * Method to set the name of the person
+     *
+     * @param newName is the new name of the person
+     */
     public void setName(String newName) {
         this.name = newName;
     }
 
+    /**
+     * Method to delete a user
+     */
     public void deleteUser() {
         ArrayList<People> people = LmsMain.getPeopleListInFile();
         for (int i = 0; i < people.size(); i++) {
@@ -58,5 +79,4 @@ public class People implements Serializable {
             }
         }
     }
-
 }
