adebfc3f4f3fceb418e3e0d0db4e9572079f964c
Dakshesh Gupta <64595877+Daksh3K@users.noreply.github.com>
2021-12-14 00:27:46 +0400

    final commit

--- Submission.java
+++ Submission.java
@@ -6,21 +6,21 @@ import java.io.*;
  * the teacher can use this class to give student's their grades on their
  * quizzes.
  *
+ * @version December 11, 2021
  * @author Yu Hyun Kim
- * @version December 11 2021
  */
 public class Submission implements Serializable {
     private String quizQuestionString;  //the questions in the format the student saw them and their answers
-    private String quizAnsweredString; //the answered quiz from the student that took it
-    private String subDateString; //the subdate of when the quiz was taken
+    private String quizAnsweredString;  //the answered quiz from the student that took it
+    private String subDateString;    //the subdate of when the quiz was taken
     private String teacherWhoCreatedQuiz;  //the email of the teacher who created the quiz
     private String gradeForStudent;  //the grade for the student
     private String nameOfQuizTaken;  //name of the quiz
     private int numOfQuestions;  //number of quiz questions
-    private String studentWhoTookQuiz; //the student who took the quiz
+    private String studentWhoTookQuiz;   //the student who took the quiz
 
     /**
-     * Constructor for Submission class
+     * constructor for Submission class
      * When submission objects are made, the gradeForStudent instance variable is instantiated to "Not graded yet"
      * However, the teacher can give grades and set the gradeForStudent instance variable to the student's grade
      *
@@ -31,18 +31,26 @@ public class Submission implements Serializable {
      * @param nameOfQuizTaken       the name of the quiz that the student takes
      * @param numOfQuestions        the number of questions in the quiz
      * @param student               the student that took the quiz
+     *
      */
-    public Submission(String quizQuestionString, String quizAnswerString, String subDateString, String teacherWhoCreatedQuiz,
+    public Submission(String quizQuestionString, String quizAnswerString,
+                      String subDateString, String teacherWhoCreatedQuiz,
                       String nameOfQuizTaken, int numOfQuestions, String student) {
         this.quizQuestionString = quizQuestionString;
         this.quizAnsweredString = quizAnswerString;
         this.subDateString = subDateString;
         this.teacherWhoCreatedQuiz = teacherWhoCreatedQuiz;
         this.nameOfQuizTaken = nameOfQuizTaken;
-        this.gradeForStudent = "Not graded yet";
+        this.gradeForStudent = "Not graded yet**";
         this.numOfQuestions = numOfQuestions;
         this.studentWhoTookQuiz = student;
     }
+
+    /**
+     * The following are getter and setter methods for the instance variables
+     * in the Submission class.
+     */
+
 
     /**
      * Method to get the name of the quiz taken
@@ -52,6 +60,7 @@ public class Submission implements Serializable {
     public String getNameOfQuizTaken() {
         return nameOfQuizTaken;
     }
+
 
     /**
      * Method to get the teacher who created the quiz
@@ -94,18 +103,14 @@ public class Submission implements Serializable {
      *
      * @return String
      */
-    public String getQuizAnsweredString() {
-        return quizAnsweredString;
-    }
+    public String getQuizAnsweredString() { return quizAnsweredString; }
 
     /**
      * Method to get the subdate of quiz
      *
      * @return String
      */
-    public String getSubDateString() {
-        return subDateString;
-    }
+    public String getSubDateString() { return subDateString; }
 
     /**
      * Method to get the number of questions in the quiz
